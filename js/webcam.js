59b30435db3a89bdb5d50f2e55b178052b9253b2
Wilson Wang <xerodragon72@gmail.com>
2017-09-23 14:08:37 -0400

    fix some bugs

--- js/webcam.js
+++ js/webcam.js
@@ -1,18 +1,15 @@
-(function () {
-    navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
+var errorCallback = function (e) {
+    console.log('Reeeejected!', e);
+};
 
-    navigator.getMedia(
-        // constraints
-        {video:true, audio:false},
+// Not showing vendor prefixes.
+navigator.getUserMedia({ video: true, audio: true }, function (localMediaStream) {
+    var video = document.getElementsByTagName('video')[0];
+    video.src = window.URL.createObjectURL(localMediaStream);
 
-        // success callback
-        function (mediaStream) {
-            var video = document.getElementsByTagName('video')[0];
-            video.src = window.URL.createObjectURL(mediaStream);
-            video.play();
-        },   
-        //handle error
-        function (error) {
-            console.log(error);
-        })   
-})();
+    // Note: onloadedmetadata doesn't fire in Chrome when using it with getUserMedia.
+    // See crbug.com/110938.
+    video.onloadedmetadata = function (e) {
+        // Ready to go. Do some stuff.
+    };
+}, errorCallback);
